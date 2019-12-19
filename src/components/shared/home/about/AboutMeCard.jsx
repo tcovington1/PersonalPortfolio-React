e82d2342b71c2d09996d5ac111274de65b2e756c
Taylor Covington <tcovingtondev@gmail.com>
2019-12-18 22:50:03 -0700

    added dpl pick to mobile

--- src/components/shared/home/about/AboutMeCard.jsx
+++ src/components/shared/home/about/AboutMeCard.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import CertificatePic from './AboutMeCertPicFull';
+
 import '../Home.scss';
 
 const AboutMeCard = () => (
@@ -11,13 +14,15 @@ const AboutMeCard = () => (
               I recently graduated from DevPoint Labs Full-Stack Web Developer Bootcamp in 2019 but
               I've been self-taught for about a year now. I love learning new technologies to build
               solutions web sites and applications. </p>
-              <br></br>
-              <p className='words-p'>I am currently learning all aspects of React.js which includes Gatsby.js and Next.js.
-              I also really enjoy learning back-end languages. During bootcamp we learned Ruby and Ruby on Rails with PostgreSQL
-              which makes building a full-stack application very easy. I'm currenly learning GraphQL
-              and Apollo. What can I say, I love to learn!
-              </p>
-          </div>
+              {/* </div> */}
+                {/* <CertificatePic style={{alignItems: 'left'}}/> */}
+              {/* <div className="words"> */}
+                <p className='words-p'>I am currently learning all aspects of React.js which includes Gatsby.js and Next.js.
+                I also really enjoy learning back-end languages. During bootcamp we learned Ruby and Ruby on Rails with PostgreSQL
+                which makes building a full-stack application very easy. I'm currenly learning GraphQL
+                and Apollo. What can I say, I love to learn!
+                </p>
+              </div>
     </>
 )
 
