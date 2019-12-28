7176b29bc3baea072bfc81ab06b265d90093b5da
Taylor Covington <tcovingtondev@gmail.com>
2019-12-28 05:52:11 -0700

    added react-typed to hero

--- src/components/shared/home/HomeHero.jsx
+++ src/components/shared/home/HomeHero.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 // import styled from 'styled-components';
 import Fade from 'react-reveal';
+import Typed from 'react-typed';
 import './Home.scss';
 
+
 const Hero = () => {
+  
+
   
   return(
     
@@ -12,13 +16,29 @@ const Hero = () => {
       <div className='center-col'>
          <Fade duration={2000}>
            <h1>Taylor <br></br> Covington</h1>
-          <h6>A Full-Stack Developer</h6>
+           <div>
+                <Typed
+                className='typed-hero'
+                strings={[
+                    'I am a Front-End Developer',
+                    'I am a Back-End Developer',
+                    'I am a Full-Stack Developer']}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    // loop
+                    >
+                </Typed>
+            </div>
         </Fade>
     </div>
   </div>
   </div>
   
   )
-}
+};
 
 export default Hero;
+
+
+
+
--- src/mobile/home/home-hero-mob/HomeHeroMob.jsx
+++ src/mobile/home/home-hero-mob/HomeHeroMob.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import styled from 'styled-components';
 import Fade from 'react-reveal';
+import Typed from 'react-typed';
 import './HomeHeroMob.scss';
 import '../HomeMob.scss';
 
@@ -13,8 +14,19 @@ const Hero = () => {
       <div className='center-col-mob'>
          <Fade duration={2000}>
            <h1>Taylor <br></br> Covington</h1>
-          <h6>A Full-Stack Developer</h6>
-        </Fade>
+           <div>
+                <Typed
+                className='typed-hero-mob'
+                strings={[
+                    "I'm a Front-End Developer",
+                    "I'm a Back-End Developer",
+                    "I'm a Full-Stack Developer"]}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    // loop
+                    >
+                </Typed>
+            </div>        </Fade>
     </div>
   </div>
   </div>
