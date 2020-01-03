a916c3e51b70cfaf0cb6c108b4f294661b2fc426
Taylor Covington <tcovingtondev@gmail.com>
2020-01-02 22:01:22 -0700

    updated blog links in header

--- src/mobile/navMob/NavBarMob.jsx
+++ src/mobile/navMob/NavBarMob.jsx
@@ -29,7 +29,7 @@ const Navbar = (props) => {
             <Link className='nav-links'to="/">Home</Link>
             <Link className='nav-links' to="/about">About</Link>
             <Link className='nav-links' to="/contact">Contact</Link>
-            <Link ><a className='nav-links' href="https://codewithtaylor.netlify.com/">Blog</a></Link>
+            <a className='nav-links' href="https://codewithtaylor.netlify.com/">Blog</a>
             <Link className='nav-links' to="/resume">Resume</Link>
           </NavLinks>
           <BurgerWrapper>
