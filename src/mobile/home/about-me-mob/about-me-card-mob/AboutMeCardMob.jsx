b4156c8a03afd2d66309a3edcf95982e99a7e7cc
Taylor Covington <tcovingtondev@gmail.com>
2020-01-11 10:50:47 -0700

    updated about me

--- src/mobile/home/about-me-mob/about-me-card-mob/AboutMeCardMob.jsx
+++ src/mobile/home/about-me-mob/about-me-card-mob/AboutMeCardMob.jsx
@@ -6,7 +6,7 @@ const AboutMeCardMob = () => (
   <>
           
           <div className="words-mob">
-          <p className='words-p'> Hey there! <br></br> I'm Taylor Covington, a full-stack developer from Salt Lake City, UT.
+          <p className='words-p'> Hey there! <br></br> I'm Taylor Covington, a full-stack developer from Salt Lake City, UT. I currently specialize in React.js and Ruby on Rails, or React on Rails!
               I'm also a husband and father of two boys!
               I recently graduated from DevPoint Labs Full-Stack Web Developer Bootcamp in 2019 but
               I've been self-taught for about a year now. I love learning new technologies to build
@@ -15,9 +15,9 @@ const AboutMeCardMob = () => (
               <CertPic />
               <br></br>
             <div className="words-mob">
-              <p className='words-p'>I am currently learning all aspects of React.js which includes Gatsby.js and Next.js.
+              <p className='words-p'>I am currently learning all aspects of React.js including Gatsby.js and GraphQL.
               I also really enjoy learning back-end languages. During bootcamp we learned Ruby and Ruby on Rails with PostgreSQL
-              which makes building a full-stack application very easy. I'm currenly learning Node.js, Express.js, and MongoDB to extend my stack knowledge and offering. What can I say, I love to learn!
+              which makes building a full-stack application very easy. I'm also dabbling in Node.js, Express.js, and MongoDB to extend my stack knowledge and offering. What can I say, I love to learn!
               </p>
           
             </div>
