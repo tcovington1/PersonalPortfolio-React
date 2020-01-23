7118a73c7b7d6701ad1bfb5a83cbe6ded75d7f70
Taylor Covington <tcovingtondev@gmail.com>
2020-01-23 13:58:19 -0700

    updating mobile image sizing for about me section

--- src/mobile/home/about-me-mob/AboutMePic.jsx
+++ src/mobile/home/about-me-mob/AboutMePic.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MyPic from '../../../assets/Taylor_Covington.jpeg';
+import MyPic from '../../../assets/Taylor_Covington_mob.jpeg';
 import './AboutMePic.scss';
 
 const AboutMePic = () => (
   <>
     <div className='img-cropper-mob'>
-      <img src={MyPic} className="about-pic-mob"/>
+      <img src={MyPic} className="about-pic-mob-main"/>
     </div>
   </>
 )
