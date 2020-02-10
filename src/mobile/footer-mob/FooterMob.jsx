5f86eca0d57be613a1333c801cfe606895669fa0
Taylor Covington <tcovingtondev@gmail.com>
2020-02-09 20:22:20 -0700

    updated social data to include linkedIn profile, added to footer. Changed font for desktop skills card

--- src/mobile/footer-mob/FooterMob.jsx
+++ src/mobile/footer-mob/FooterMob.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from '../../components/Button'
 import socialLinks from './SocialData';
 
 import SocialLinkMob from './SocialLink';
@@ -17,8 +18,21 @@ const FooterMob = () => {
     <>
       <div className='social-flex-cont-mob'>
         <div className="foot-cp-mob">
-          <h6>&copy; 2019 Taylor J Covington</h6>
+          <h6>&copy; 2020 Taylor J Covington</h6>
         </div>
+        {/* <div className="foot-cp-mob">
+          <h6>Built using React</h6>
+        </div> */}
+        {/* <div className="footer-repo">
+          <h6>Built using React</h6>
+          <h6>Portolio <a href="">GitHub Repo</a></h6>
+        </div> */}
+        {/* <Button
+        type='button'
+        buttonStyle='butn-primary-outline'
+        buttonSize='butn-xsm'
+      ><a href='https://codewithtaylor.com/' className='button-link'>Blog</a>
+      </Button> */}
         <div className="social-mob">
 
         {socialLinks.map(mapSocial)}
