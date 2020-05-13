07460f18a62881ccd069502d6aa67dc35c295149
Taylor Covington <tcovingtondev@gmail.com>
2020-05-12 18:14:32 -0600

    updated spelling error in about me card

--- src/mobile/home/about-me-mob/about-me-card-mob/AboutMeCardMob.jsx
+++ src/mobile/home/about-me-mob/about-me-card-mob/AboutMeCardMob.jsx
@@ -6,7 +6,7 @@ const AboutMeCardMob = () => (
   <>
     
     <div className="words-mob">
-    <p className='words-p'> Hey there! <br></br> I'm Taylor Covington, a <strong>Front-End Developer</strong> from Salt Lake City, UT. I am passionate about building effective websites using technologies such as <strong>React.js</strong>. I develope effective websites by focusing on <strong>user experience</strong>, <strong>SEO</strong>, and <strong>A/B testing</strong>.
+    <p className='words-p'> Hey there! <br></br> I'm Taylor Covington, a <strong>Front-End Developer</strong> from Salt Lake City, UT. I am passionate about building effective websites using technologies such as <strong>React.js</strong>. I develop effective websites by focusing on <strong>user experience</strong>, <strong>SEO</strong>, and <strong>A/B testing</strong>.
         I'm also a husband and father of two boys!
         I recently graduated from <strong>DevPoint Labs Full-Stack Web Developer Bootcamp</strong> in Nov 2019 and finished a three month
         <strong>full-stack internship with Generations Humanitairan</strong> in December 2019. </p>
